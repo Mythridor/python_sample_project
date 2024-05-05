093702a7f6149fd926b950eb0b0f44cbcc1b712b
Mythridor <enoubia@gmail.com>
2024-05-05 16:49:51 +0200

    chores: Setup flake8 and pytest

---
+++ sample_project/main.py
@@ -0,0 +1,18 @@
+from fastapi import (
+    FastAPI,
+    __version__
+)
+from enum import Enum
+
+app = FastAPI()
+
+
+class Status(Enum):
+    OK = "OK"
+    NOK = "NOK"
+
+
+@app.get("/healthz")
+def is_up():
+    status = (__version__ == '0.111.0') * Status.OK or Status.NOK
+    return {"Status": status}
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/healthz")
-def is_up():
-    return {"Status": "OK"}
